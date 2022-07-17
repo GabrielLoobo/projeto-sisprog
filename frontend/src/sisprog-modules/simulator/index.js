11be60a71064d0d2314dd16a24ef2836efeeb505
Gabriel Lobo <gabriellobodurante@gmail.com>
2022-07-17 02:21:12 -0300

    adiciona ExecutionSimulator

--- frontend/src/sisprog-modules/simulator/index.js
+++ frontend/src/sisprog-modules/simulator/index.js
@@ -1,8 +1,21 @@
 import Memory from './components/memory'
+import Register from './components/register'
 
 export function test_memory() {
     const memory = new Memory()
     console.log(memory.get_at(0))
     memory.set_at(0, 2**8)
     console.log(memory.get_at(0))
+}
+
+export default class ExecutionSimulator {
+    _memory;
+    _PC;
+    _RD
+
+    constructor(){
+        this._memory = new Memory();
+        this._PC = new Register();
+        this._RD = new Register();
+    }
 }
