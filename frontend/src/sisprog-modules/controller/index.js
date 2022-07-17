b2261a588306019b6d6fc090b02619cd04aa3427
Gabriel Lobo <gabriellobodurante@gmail.com>
2022-07-17 03:25:31 -0300

    adiciona comentários para ajudar a desenvolver o controller + montador

--- frontend/src/sisprog-modules/controller/index.js
+++ frontend/src/sisprog-modules/controller/index.js
@@ -8,6 +8,12 @@ export default class Controller {
     }
 
     setupExecution(rawInputCode) {
+        // Recebe o código do front end como string
+
+        // Fazer: "Limpar" o código de comentários/coisas inúteis (baixa prioridade)
+        // Fazer: Chamar o montador, passando o código recebido em rawInputCode e recebendo o código binário que será colocado na memória
+        
+        console.log(rawInputCode)
         console.log('Setup Execution')
     }
 }
